e95ac12bec4cadb124f97e514633fa9ea849d7d1
deepak891 <deepak891@gmail.com>
2016-12-13 00:06:00 +0800

    Project Setup - Initial Commit

---
+++ golabsrailway-api/src/main/java/com/golabsrailway/api/App.java
@@ -0,0 +1,13 @@
+package com.golabsrailway.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
